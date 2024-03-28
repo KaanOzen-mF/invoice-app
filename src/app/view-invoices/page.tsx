230fe6282fd322b82ecf823acfbd2e2acd795ea9
KaanOzen-mF <kaanozn98@gmail.com>
2024-03-28 11:48:08 +0300

    Add invoices and view invoices pages

---
+++ src/app/view-invoices/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface Props {
+  // Define your component props here
+}
+
+const Page: React.FC<Props> = (props) => {
+  return (
+    <div>
+      <h1>View Invoices Page</h1>
+    </div>
+  );
+};
+
+export default Page;
